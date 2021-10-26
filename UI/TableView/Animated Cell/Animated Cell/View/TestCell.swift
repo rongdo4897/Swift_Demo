b1a0dd233c4fc5bf0518b6b4acfd9cf06babd3e4
Hoang Lam <tunglam977@gmail.com>
2021-10-26 11:01:37 +0700

    (feat) done

---
+++ UI/TableView/Animated Cell/Animated Cell/View/TestCell.swift
@@ -0,0 +1,24 @@
+//
+//  TestCell.swift
+//  Animated Cell
+//
+//  Created by Hoang Tung Lam on 1/26/21.
+//
+
+import UIKit
+
+class TestCell: UITableViewCell {
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
