132d6df504700717b3bb91f2eff0febeae924de0
Hoang Lam <tunglam977@gmail.com>
2021-12-31 11:57:28 +0700

    (feat) demo config enviroment

---
+++ 2 - Other/Enviroment/Config Enviroment/Config Enviroment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Config Enviroment
+//
+//  Created by Hoang Lam on 31/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(Environment.rootURL)
+    }
+
+
+}
+
